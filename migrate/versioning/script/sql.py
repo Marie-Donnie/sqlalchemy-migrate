25bda263fc8aedf2f62c5409986d1d66c69e7ba3
Marie-Donnie <delavergnemarie@gmail.com>
2018-03-01 16:25:33 +0100

    Added retries to migrations

--- migrate/versioning/script/sql.py
+++ migrate/versioning/script/sql.py
@@ -3,11 +3,13 @@
 import logging
 import re
 import shutil
+import time
 
 import sqlparse
 
 from migrate.versioning.script import base
 from migrate.versioning.template import Template
+import psycopg2.extensions
 
 
 log = logging.getLogger(__name__)
@@ -35,36 +37,43 @@ class SqlScript(base.BaseScript):
         # starts with a comment?)
         conn = engine.connect()
         try:
-            trans = conn.begin()
-            try:
-                # ignore transaction management statements that are
-                # redundant in SQL script context and result in
-                # operational error being returned.
-                #
-                # Note: we don't ignore ROLLBACK in migration scripts
-                # since its usage would be insane anyway, and we're
-                # better to fail on its occurance instead of ignoring it
-                # (and committing transaction, which is contradictory to
-                # the whole idea of ROLLBACK)
-                ignored_statements = ('BEGIN', 'END', 'COMMIT')
-                ignored_regex = re.compile('^\s*(%s).*;?$' % '|'.join(ignored_statements),
-                                           re.IGNORECASE)
+            retries = 0
+            while(true):
+                trans = conn.begin()
+                try:
+                    # ignore transaction management statements that are
+                    # redundant in SQL script context and result in
+                    # operational error being returned.
+                    #
+                    # Note: we don't ignore ROLLBACK in migration scripts
+                    # since its usage would be insane anyway, and we're
+                    # better to fail on its occurance instead of ignoring it
+                    # (and committing transaction, which is contradictory to
+                    # the whole idea of ROLLBACK)
+                    ignored_statements = ('BEGIN', 'END', 'COMMIT')
+                    ignored_regex = re.compile('^\s*(%s).*;?$' % '|'.join(ignored_statements),
+                                               re.IGNORECASE)
 
-                # NOTE(ihrachys): script may contain multiple statements, and
-                # not all drivers reliably handle multistatement queries or
-                # commands passed to .execute(), so split them and execute one
-                # by one
-                text = sqlparse.format(text, strip_comments=True, strip_whitespace=True)
-                for statement in sqlparse.split(text):
-                    if statement:
-                        if re.match(ignored_regex, statement):
-                            log.warning('"%s" found in SQL script; ignoring' % statement)
-                        else:
-                            conn.execute(statement)
-                trans.commit()
-            except Exception as e:
-                log.error("SQL script %s failed: %s", self.path, e)
-                trans.rollback()
-                raise
+                    # NOTE(ihrachys): script may contain multiple statements, and
+                    # not all drivers reliably handle multistatement queries or
+                    # commands passed to .execute(), so split them and execute one
+                    # by one
+                    text = sqlparse.format(text, strip_comments=True, strip_whitespace=True)
+                    for statement in sqlparse.split(text):
+                        if statement:
+                            if re.match(ignored_regex, statement):
+                                log.warning('"%s" found in SQL script; ignoring' % statement)
+                            else:
+                                conn.execute(statement)
+                    trans.commit()
+                    return
+                except psycopg2.extensions.TransactionRollbackError as e:
+                    retries += 1
+                    time.sleep(2**retries)
+                    trans.rollback()
+                except Exception as e:
+                    log.error("SQL script %s failed: %s", self.path, e)
+                    trans.rollback()
+                    raise
         finally:
             conn.close()
